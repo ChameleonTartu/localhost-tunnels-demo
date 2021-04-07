2dea55ae3cddc3e5712e20bece19ed5a6610f8fa
Dmytro Chasovskyi <dmytro.chasovskyi@greenbird.com>
2021-04-07 13:02:18 +0200

    Add logging in Scheduled annotation

--- src/main/kotlin/com/greenbird/localhosttunnelsdemo/service/CallbackProcessorService.kt
+++ src/main/kotlin/com/greenbird/localhosttunnelsdemo/service/CallbackProcessorService.kt
@@ -2,6 +2,7 @@ package com.greenbird.localhosttunnelsdemo.service
 
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class CallbackProcessorService {
@@ -18,6 +19,7 @@ class CallbackProcessorService {
 
     @Scheduled(fixedDelay = 3000)
     fun invokeCallbacks() {
+        println("Invoke scheduled callbacks: ${LocalDateTime.now()}")
         val notification = "{\"created\": true}"
 
         for (callback in callbacks) {
